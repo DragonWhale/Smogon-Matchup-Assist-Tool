6ed7cc4c3e0e7dbdafb45f72218d27f112eb8855
DragonWhale <alex-a93@hotmail.com>
2021-01-23 16:43:30 +0900

    Improved Interface

--- quiz.js
+++ quiz.js
@@ -35,13 +35,13 @@ function loadQuestion() {
   var team = teamlist[selection[question]];
 
   document.getElementById('centerarea').innerHTML = '<table><tr><th>' + team[0] + '</th></tr><tr><td><i class="icon icon-' + team[2] + '"></i><span> Lv.50</span></td></tr><tr><td><i class="icon icon-' + team[3] + '"></i><span> Lv.50</span></td></tr><tr><td><i class="icon icon-' + team[4] + '"></i><span> Lv.50</span></td></tr><tr><td><i class="icon icon-' + team[5] + '"></i><span> Lv.50</span></td></tr><tr><td><i class="icon icon-' + team[6] + '"></i><span> Lv.50</span></td></tr><tr><td><i class="icon icon-' + team[7] + '"></i><span> Lv.50</span></td></tr>';
-  document.getElementById('bottomarea').innerHTML = '<h5>Current question: ' + (question + 1) + '</h5><h5> Season: ' + team[1] + '</h5><h5>Current Score: ' + score + '</h5><h5>Previous result: ' + previousAns + '</h5>';
+  document.getElementById('bottomarea').innerHTML = '<h5>Current question: ' + (question + 1) + '</h5><h5>Current Score: ' + score + '</h5><h5>Previous result: ' + previousAns + '</h5>';
 
   answer = team[8];
 }
 
 function loadResults() {
-  document.getElementById('centerarea').innerHTML = '<h4>You scored <br> ' + score + ' out of 10!</h4>';
+  document.getElementById('centerarea').innerHTML = '<h1>You scored <br> ' + score + ' out of 10!</h1>';
   gameRunning = false;
   document.getElementById('bottomarea').innerHTML = '<span><h1 style="color: white;"> TRY AGAIN <h1></span>'
 }
